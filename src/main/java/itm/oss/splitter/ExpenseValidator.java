77bca367494c44530df0d5c0dd7fa975c7b79abd
ichbinju0 <hahalove79@naver.com>
2025-10-22 22:37:04 +0900

    hi

--- src/main/java/itm/oss/splitter/ExpenseValidator.java
+++ src/main/java/itm/oss/splitter/ExpenseValidator.java
@@ -2,7 +2,9 @@ package itm.oss.splitter;
 
 public class ExpenseValidator {
   public static void validate(Expense e) {
-    // TODO (Issue 3): implement checks (payer nonblank, amount > 0, participants nonempty, currency nonblank)
+    // TODO (Issue 3): implement checks (payer nonblank, amount > 0, participants
+    // nonempty, currency nonblank)
     // Keep empty for Phase 1.
+    System.out.println("hello!");
   }
 }
