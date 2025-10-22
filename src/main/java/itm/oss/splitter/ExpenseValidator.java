6940dc5e0f0cf19b7c6dcb7858df2c03eaa686b8
ichbinju0 <hahalove79@naver.com>
2025-10-22 23:28:53 +0900

    I check payer, amount and currency

--- src/main/java/itm/oss/splitter/ExpenseValidator.java
+++ src/main/java/itm/oss/splitter/ExpenseValidator.java
@@ -1,12 +1,23 @@
 package itm.oss.splitter;
 
+import java.math.BigDecimal;
+
 public class ExpenseValidator {
   public static void validate(Expense e) {
     // TODO (Issue 3): implement checks (payer nonblank, amount > 0, participants
     // nonempty, currency nonblank)
     // Keep empty for Phase 1.
     if (e.getPayer().isEmpty()) {
-      System.err.println("error!");
+      throw new IllegalArgumentException("Payer is Empty! Pleases add payer");
     }
+
+    if (e.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
+      throw new IllegalArgumentException("Amount should be larger than 0");
+    }
+
+    if (e.getCurrency().isEmpty()) {
+      throw new IllegalArgumentException("Currency is Empty! Please add currency");
+    }
+
   }
 }
