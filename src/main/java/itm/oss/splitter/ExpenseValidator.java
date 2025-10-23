b3c316d562e2f1808b091880ddbe106d2caff77f
ichbinju0 <hahalove79@naver.com>
2025-10-23 20:47:08 +0900

    modify comments

--- src/main/java/itm/oss/splitter/ExpenseValidator.java
+++ src/main/java/itm/oss/splitter/ExpenseValidator.java
@@ -7,34 +7,33 @@ public class ExpenseValidator {
     // TODO (Issue 3): implement checks (payer nonblank, amount > 0, participants
     // nonempty, currency nonblank)
 
-    // Checking if payer is empty
+    // check if payer is nonblank and not null
     if (e == null || e.getPayer().isBlank()) {
-      // modify isEmpty -> isBlank: for more safe check !!
       throw new IllegalArgumentException("Payer is Empty! Please add payer");
     }
 
-    // Checking if amount is larger than 0
+    // check if amount is bigger than zero and amount is not null
     if (e == null || e.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
       throw new IllegalArgumentException("Amount should be larger than 0");
     }
 
-    // Checking if currency is empty
+    // check if currency is nonblank and not null
     if (e == null || e.getCurrency().isBlank()) {
       throw new IllegalArgumentException("Currency is Empty! Please add currency");
     }
 
-    // Checking if the list(participants) is not empty
+    // check if participants size is bigger or same than 1 and if names are nonblank
+    // and not null
     if (e == null || e.getParticipants().isEmpty()) {
       throw new IllegalArgumentException("Participants list cannot be empty.");
     }
-
-    // Checking if participant in the list(participants) is empty
     for (String participant : e.getParticipants()) {
       if (e == null || participant.isBlank()) {
         throw new IllegalArgumentException("Participant name cannot be blank.");
       }
     }
-    // Checking if date is blank
+
+    // check if date is nonblank and not null
     if (e == null || e.getDate().isBlank()) {
       throw new IllegalArgumentException("Date cannot be blank.");
     }
