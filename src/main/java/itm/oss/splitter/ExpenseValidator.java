7fd22f61a335cf8ec5f4e14d0e0446a2ec95efbb
ichbinju0 <hahalove79@naver.com>
2025-11-04 19:18:18 +0900

    I made a method to check if e is null

--- src/main/java/itm/oss/splitter/ExpenseValidator.java
+++ src/main/java/itm/oss/splitter/ExpenseValidator.java
@@ -7,35 +7,40 @@ public class ExpenseValidator {
     // TODO (Issue 3): implement checks (payer nonblank, amount > 0, participants
     // nonempty, currency nonblank)
 
+    // null check for expense
+    if (e == null) {
+      throw new IllegalArgumentException("Expense cannot be null.");
+    }
+
     // check if payer is nonblank and not null
-    if (e == null || e.getPayer().isBlank()) {
+    if (e.getPayer().isBlank()) {
       throw new IllegalArgumentException("Payer is Empty! Please add payer");
 
     }
 
     // check if amount is bigger than zero and amount is not null
-    if (e == null || e.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
+    if (e.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
       throw new IllegalArgumentException("Amount should be larger than 0");
     }
 
     // check if currency is nonblank and not null
-    if (e == null || e.getCurrency().isBlank()) {
+    if (e.getCurrency().isBlank()) {
       throw new IllegalArgumentException("Currency is Empty! Please add currency");
     }
 
     // check if participants size is bigger or same than 1 and if names are nonblank
     // and not null
-    if (e == null || e.getParticipants().isEmpty()) {
+    if (e.getParticipants().isEmpty()) {
       throw new IllegalArgumentException("Participants list cannot be empty.");
     }
     for (String participant : e.getParticipants()) {
-      if (e == null || participant.isBlank()) {
+      if (participant.isBlank()) {
         throw new IllegalArgumentException("Participant name cannot be blank.");
       }
     }
 
     // check if date is nonblank and not null
-    if (e == null || e.getDate().isBlank()) {
+    if (e.getDate().isBlank()) {
       throw new IllegalArgumentException("Date cannot be blank.");
     }
 
